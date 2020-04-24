685f2ad0e095082b07a7865a83fb9efa84472138
ohjacksn <jacksonmo@me.com>
2020-04-24 09:25:16 -0400

    lab update

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
+    create_table :users do |x|
+      x.string :username
+      x.string :password_digest
+    end
   end
   
   def down
+    drop_table :users
   end
 end
